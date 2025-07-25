7bf5354d949c679ee3f0492e65c2e24b312ff117
Marcosauras <44006538+Marcosauras@users.noreply.github.com>
2025-07-25 13:46:42 -0500

    Forms getting added

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+
+import "./global.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+
+const App = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+      <Routes>
+        {/* Public */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* Private Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
